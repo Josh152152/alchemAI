52f78e3c55d62c385616458fc1115744724ec3ed
Josh152152 <sebastien.giard.fr@gmail.com>
2025-07-14 19:50:35 +0200

    Update sheets.py

--- app/sheets.py
+++ app/sheets.py
@@ -1,1 +1,24 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+import json
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open('JobAdAgentData').sheet1  # Change to your sheet name
+
+FIELDS = [
+    "job_title", "responsibilities", "compensation_range", "benefits", "work_life_balance",
+    "company_culture", "team_size", "ideal_candidate_profile", "required_skills", "growth_opportunity",
+    "company_values", "workspace_type", "unique_perks", "hiring_timeline", "candidate_type", "key_projects",
+    "probation_details", "experience_level", "working_schedule", "location_preferences", "certifications"
+]
+
+def store_to_gsheet(job_json):
+    """Append a new row to the GSheet with the latest JD data."""
+    if isinstance(job_json, str):
+        job_json = json.loads(job_json)
+    values = [job_json.get(field, "") for field in FIELDS]
+    sheet.append_row(values)
 
